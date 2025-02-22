024a58e8f4bd34af798863bcfa8f7c896fb14296
Cohdur <starzecnorbert37@gmail.com>
2025-02-22 09:19:15 -0600

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,32 @@
+
+public class Circle
+{
+    private double radius;
+
+    public Circle()
+    {
+        radius = 1.0;
+    }
+
+    public Circle(double r)
+    {
+        radius = r;
+    }
+
+
+    public double getRadius()
+    {
+        return radius;
+    }
+
+
+    public double getArea()
+    {
+        return radius * radius * Math.PI;
+    }
+    
+    public double getPerimeter()
+    {
+        return 2 * radius * Math.PI;
+    }
+}
